78e0a75df9e6ef3e7cea973955c9cb4e595fb96e
Jaime <100472248@alumnos.uc3m.es>
2024-04-11 19:25:17 +0200

    funcion logged y submenus usarios

--- www/client/scripts/client.js
+++ www/client/scripts/client.js
@@ -1,1 +1,42 @@
 const socket = io();
+
+//-----SECCIÓN BÚSQUEDA-MENÚS---------------
+let sesion = 0;
+function logged(){
+    alert(sesion);
+    if (sesion == 0){
+          window.location.href = "./client/client_login.html";
+        }
+        else{
+          window.location.href = "./client/client_user_profile.html";
+        }
+}
+
+
+//-----SECCIÓN AJUSTES DE USUARIO------------
+document.getElementById("user_settings").style.display = "none";
+document.getElementById("user_open").style.transform = "none";
+document.getElementById("not_settings").style.display = "none";
+document.getElementById("not_open").style.transform = "none";
+
+
+function settingsCat(id){
+    let bot = document.getElementById(id);
+    let submenu;
+    if (id == "user_open"){
+        submenu = document.getElementById("user_settings");
+    }
+    if (id == "not_open"){
+        submenu = document.getElementById("not_settings");
+    }
+    let rotation = bot.style.transform;
+    if (rotation == "none"){
+        submenu.style.display = "block";
+        bot.style.transform = 'rotate(90deg)';
+    }
+    else{
+        submenu.style.display = "none";
+        bot.style.transform = 'none';
+        
+    }
+}
--- www/index.js
+++ www/index.js
@@ -1,5 +1,17 @@
 const socket = io();
 
+//-----SECCIÓN BÚSQUEDA-MENÚS---------------
+let sesion = 0;
+function logged(){
+    alert(sesion);
+    if (sesion == 0){
+          window.location.href = "./client/client_login.html";
+        }
+        else{
+          window.location.href = "./client/client_user_profile.html";
+        }
+}
+
 
 
 socket.on("connect", () => {
