fa2c0e29f4e417fc8489c0509ea0813c291bfcdc
Jaime <100472248@alumnos.uc3m.es>
2024-04-19 13:23:23 +0200

    chart y logged move

--- www/client/scripts/movements.js
+++ www/client/scripts/movements.js
@@ -1,30 +1,19 @@
-let posicion_inicial = { alpha: -1, beta: -1, gamma: -1 };
-let aceleracion = 0;
-let radio = 0.25; //Radio aprox. de un movil
+let posicion_inicial = { alpha: DeviceOrientationEvent.alpha, beta: DeviceOrientationEvent.beta, gamma: DeviceOrientationEvent.gamma };
 
-window.addEventListener("devicemotion", function(event){
-    let posicion_actual = event.rotationRate;
-    let v_aceleracion = event.acceleration;
-    let tiempo = event.interval*0.001;//En segundos
-    
-    aceleracion = Math.round(Math.sqrt(v_aceleracion.x ** 2 + v_aceleracion.y ** 2 + v_aceleracion.z ** 2), 2);
+window.addEventListener("deviceorientation", function(event){
+    let posicion_actual ={ alpha: event.alpha, beta: event.beta, gamma: event.gamma };
+    console.log("hola");
     
     if (posicion_inicial.alpha ===-1) {
-        posicion_inicial.alpha = posicion_actual.alpha;
-        posicion_inicial.beta = posicion_actual.beta;
-        posicion_inicial.gamma = posicion_actual.gamma;
+        posicion_inicial.alpha = event.alpha;
+        posicion_inicial.beta = event.beta;
+        posicion_inicial.gamma = event.gamma;
     } else {
-        let distancia_z = Math.round(radio*(posicion_actual.alpha)*tiempo - radio*(posicion_inicial.alpha)*tiempo, 2);
-        let distancia_y = Math.round(radio*(posicion_actual.gamma)*tiempo - radio*(posicion_inicial.gamma)*tiempo, 2);
-        let distancia_x = Math.round(radio*(posicion_actual.beta)*tiempo - radio*(posicion_inicial.beta)*tiempo, 2);
-        if (distancia_y > 0.5){
-            this.alert(sesion);
-            logged();
-            
-        } else if (distancia_x > 0.5){
-            mostrarMensaje("Eje x movido" + distancia_x);
-        } else if (distancia_z > 0.5){
+        if ((posicion_actual.gamma - posicion_inicial.gamma)> 40){
             chart_in();
+        }
+        else if ((posicion_actual.beta - posicion_inicial.beta)> 40){
+            logged();
         }
     }
     posicion_inicial.alpha = posicion_actual.alpha;
