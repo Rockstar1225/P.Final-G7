07c15c8d4cd1842fd972ce85699a7bb5ab3f5382
Jaime <100472248@alumnos.uc3m.es>
2024-04-21 11:17:32 +0200

    por fin

--- www/client/scripts/movements.js
+++ www/client/scripts/movements.js
@@ -1,34 +1,33 @@
-let angulo_inicial = {alpha: 0, beta: 0, gamma: 0};
+let angulo_inicial = {alpha: 0, beta: 90, gamma: 0};
 let angulo_actual;
 let touch_time = false;
 
 function movimientos(event){
     angulo_actual = {alpha: event.alpha, beta: event.beta, gamma: event.gamma};
-    
-    if (angulo_inicial.alpha === 0){
-        angulo_inicial = angulo_actual;
-        return;
-    }
-    
-    let dif_angulo_y = angulo_actual.gamma - angulo_inicial.gamma;
-    let dif_angulo_z = angulo_actual.alpha - angulo_inicial.alpha;
-    let dif_angulo_x = angulo_actual.beta - angulo_inicial.beta;
-    console.log(dif_angulo_x, dif_angulo_y, dif_angulo_z);
-    
-    if (dif_angulo_y < -45){
-        alert("op1");
-        logged();
-    }
-    else if (dif_angulo_x > 45){
+    console.log(angulo_actual.beta, angulo_actual.gamma, angulo_actual.alpha);
+    if (angulo_actual.gamma <= -45){
         alert("op2");
         chart_in();
     }
-    else if (dif_angulo_y < -45 && Math.abs(dif_angulo_x) < 10){
+    if (angulo_actual.beta <= 45){
+        alert("op1");
+        logged();
+    }
+    if (angulo_actual.gamma >= 45){
         alert("op3");
         window.location.href = "./client_busqueda.html";
     }
+    /*else if (dif_angulo_x > 45 && dif_angulo_y > 45 && Math.abs(dif_angulo_z) < 10){
+        console.log(dif_angulo_x, dif_angulo_y, dif_angulo_z);
+        alert("op2");
+        chart_in();
+    }
+    else if (dif_angulo_x < -45 && dif_angulo_y < -45 && Math.abs(dif_angulo_z) < 10){
+        console.log(dif_angulo_x, dif_angulo_y, dif_angulo_z);
+        alert("op3");
+        window.location.href = "./client_busqueda.html";
+    }*/
     
-    angulo_inicial = angulo_actual;
 }
 
 let boton = document.getElementById("movement");
@@ -40,7 +39,7 @@ boton.addEventListener("touchend", function(){
     touch_time = false;
 });
 
-window.addEventListener("deviceorientation", function(event){
+window.addEventListener("deviceorientationabsolute", function(event){
     if(touch_time){
         movimientos(event);
     }
